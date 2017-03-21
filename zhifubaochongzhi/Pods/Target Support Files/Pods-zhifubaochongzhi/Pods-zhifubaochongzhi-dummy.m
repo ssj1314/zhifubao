1693c4f91f0a2ee553099fa43ae6579de834f279
ssj <942980758@qq.com>
2017-03-21 17:44:13 +0800

    不知道 改啥了
    不知道 改啥了

--- zhifubaochongzhi/Pods/Target Support Files/Pods-zhifubaochongzhi/Pods-zhifubaochongzhi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_zhifubaochongzhi : NSObject
-@end
-@implementation PodsDummy_Pods_zhifubaochongzhi
-@end
--- zhifubaochongzhi/zhifubaochongzhi/ViewController.m
+++ zhifubaochongzhi/zhifubaochongzhi/ViewController.m
@@ -12,7 +12,7 @@
 #import "SSJbutton.h"// 自定义的button
 #import "MBProgressHUD.h"
 
-#import "MBProgressHUD+MJ.h"
+//#import "MBProgressHUD+MJ.h"
 
 #define ABColor(R, G, B, Alpha) [UIColor colorWithRed:(R)/255.0 green:(G)/255.0 blue:(B)/255.0 alpha:(Alpha)]
 #define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
